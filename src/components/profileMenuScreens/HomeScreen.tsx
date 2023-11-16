85e3692ba65fbba9e7a2230261b285702ad29127
Rafael Vendramini <72450490+rafaelbvc@users.noreply.github.com>
2023-11-16 18:02:23 -0300

    v. 0.3.8 - HotFix home page / svg component

--- src/components/profileMenuScreens/HomeScreen.tsx
+++ src/components/profileMenuScreens/HomeScreen.tsx
@@ -23,20 +23,20 @@ const HomeScreen: React.FC = () => {
             {!videoLoaded ? <PreloaderScreen /> : null}
             <PreloadableVideo videoPath={moviePath} onVideoLoaded={handleVideoLoad} />
             <NavBar classNameRBVCLogoBtn="text-lightGray" classNameGetStarted="text-lightGray" />
-            <section className="flex justify-center paddingYScreens z-10 flex-col h-[80%] items-center">
-                <div className="flex flex-col p-2 sm:flex-row margins ">
-                    <span className="p-3">
-                        <p className="text-lg text-center whitespace-normal text-lightGray animate-pingText" style={{ lineHeight: "1.2" }}>
+            <section className="flex justify-center paddingYScreens flex-col h-[80%] items-center">
+                <section className="flex flex-col p-2 sm:flex-row margins ">
+                    <div className="p-3 animate-pingText">
+                        <p className="text-lg text-center whitespace-normal text-lightGray " style={{ lineHeight: "1.2" }}>
                             Hello, my name is Rafael, I am the developer of this page where you are welcome! Feel free to browse,<br /> my contact details are in the contact section.<br /><br />
                             Project intended as a portfolio and personal description in technical and personal skills.
                         </p>
-                    </span>
+                    </div>
                     <div className="p-3 animate-pingIMG">
                         <UnderConstructionSVG width={"100%"} fillColor="#FCFCFF" />
                     </div>
-                </div>
+                </section>
             </section>
-            <MenuBottom className="z-10 top-[88%]" />
+            <MenuBottom className="z-10 top-[94%]" />
         </article>
     );
 };
